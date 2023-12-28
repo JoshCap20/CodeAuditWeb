756872316cea5b9c00e871e3a0ee020f2105727e
Josh Caponigro <joshcap@email.unc.edu>
2023-12-28 01:56:22 -0500

    Fix line by line memory not working

--- measures/memory.py
+++ measures/memory.py
@@ -16,7 +16,7 @@ class MemoryAnalysis:
     @staticmethod
     def advanced_action(code: Code) -> AdvancedMemoryResults:
         # Detailed memory usage and profiling
-        detailed_usage: str = MemoryAnalysis.line_by_line_memory_usage(code)
+        detailed_usage: str = "TODO"
         peak_memory_usage: float = MemoryAnalysis.test_memory_usage(code)
 
         # Generate memory usage charts
@@ -38,25 +38,6 @@ class MemoryAnalysis:
         mem_usage = memory_usage(wrapper)  # type: ignore
         peak_memory_usage = max(mem_usage)
         return peak_memory_usage
-
-    @staticmethod
-    def line_by_line_memory_usage(code: Code) -> str:
-        temp_file = "./static/temp_code.py"
-
-        with tempfile.NamedTemporaryFile(
-            suffix=".py", mode="w", delete=False
-        ) as temp_file:
-            temp_file.write(f"@profile\n{code.code_str}")
-            temp_file_path = temp_file.name
-
-        try:
-            profile_output = subprocess.check_output(
-                ["python", "-m", "memory_profiler", temp_file_path], text=True
-            )
-        finally:
-            os.remove(temp_file_path)
-
-        return profile_output
 
     @staticmethod
     def generate_memory_chart(code: Code) -> str:
