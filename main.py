4f733c5bf060601247b0721cf022125b19cf8c24
Josh Caponigro <joshcap@email.unc.edu>
2023-12-28 00:19:20 -0500

    Simplify FastAPI routing

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
-from fastapi import FastAPI, HTTPException
-from starlette.responses import FileResponse
+from fastapi import FastAPI, HTTPException, staticfiles
+from fastapi.responses import FileResponse
 
 from models import CodeRequest, Results
 from measures import PerformanceAnalyzer
 
+from utils.logger import get_logger
+
+logger = get_logger(__name__)
+
 app = FastAPI()
 
+# Static Files
+app.mount("/static", staticfiles.StaticFiles(directory="static"), name="static")
+
+# Routes
 @app.get("/")
-def serve_frontend():
+def index():
     try:
-        return FileResponse("./static/index.html")
+        return FileResponse("static/index.html")
     except Exception as e:
+        logger.error("Error while serving static/index.html")
         raise HTTPException(status_code=500, detail=str(e))
 
-@app.get("/flamegraph")
-def serve_flamegraph():
-    try:
-        return FileResponse("./static/flamegraph.svg")
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=str(e))
-
 @app.post("/analyze", response_model=Results)
-def process_code(request: CodeRequest) -> Results:
+async def process_code(request: CodeRequest) -> Results:
     try:
         return PerformanceAnalyzer.measure(request)
-    # Except frontend model validation
-    # Exception backend model validation
-    except Exception as e:
+    # TODO: Improve error handling
+    except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
+    except RuntimeError as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app)
